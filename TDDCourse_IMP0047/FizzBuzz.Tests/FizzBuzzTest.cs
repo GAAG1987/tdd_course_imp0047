9654f49c1d7d6cb4a2a7dd6bf8a9a837b00e316a
GAAG1987 <gaag1987@gmail.com>
2013-11-26 16:15:55 -0600

    primera vez que se guarda

---
+++ TDDCourse_IMP0047/FizzBuzz.Tests/FizzBuzzTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FizzBuzz.Tests
+{
+    [TestFixture]
+    public class FizzBuzzTest
+    {
+        [Test]
+        public void Answer_InputEqual1_OutputEqual01()
+        {
+            string expectedOutPut = "1";
+            FizzBuzz fb = new FizzBuzz();
+            string output = fb.Answer(1);
+            Assert.AreEqual(expectedOutPut, output);
+
+        }
+    }
+}
---
+++ TDDCourse_IMP0047/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public string Answer(int input)
+        {
+
+            return input.ToString();
+        }
+    }
+}
