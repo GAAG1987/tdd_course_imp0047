9de74f5d3a11f9c2065c93e5e1c60e89ca578de9
GAAG1987 <gaag1987@gmail.com>
2013-11-27 08:38:35 -0600

    cuarta salvada

---
+++ TDDCourse_IMP0047/CodeBreaker.Tests/CodeBreakerTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CodeBreaker.Tests
+{
+        [TestFixture]
+        public class CodeBreakerTests
+        {
+
+            public  CodeBreaker _CodeBreaker;
+
+            [SetUp] //NUnit Debe de ejecutar este método antes de cada prueba
+            public void Setup()
+            {
+                this._CodeBreaker = new CodeBreaker();
+            }
+
+            [Test]
+            
+            [TestCase("1234", "4")]
+            [TestCase("1233", "falso")]
+
+
+
+            public void Answer_InputEqualValue_OutputCorrect(String input, string expectedOutPut)
+            {
+                string output = this._CodeBreaker.Answer(input);
+
+                Assert.AreEqual(expectedOutPut, output);
+            }
+
+        }
+
+}
---
+++ TDDCourse_IMP0047/CodeBreaker/CodeBreaker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBreaker
+{
+    public class CodeBreaker
+    {
+        public string Answer(string input)
+        {
+            if(input.Length==4)
+            {
+                return "4";
+            }
+
+
+
+            return input.Length.ToString();
+        }
+
+    }
+}
