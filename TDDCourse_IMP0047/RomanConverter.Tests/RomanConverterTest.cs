b6519bf242b604ef24a93faf2af8cfe1c26d8072
GAAG1987 <gaag1987@gmail.com>
2013-11-26 18:40:34 -0600

    tercer cambio

---
+++ TDDCourse_IMP0047/RomanConverter.Tests/RomanConverterTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+namespace RomanConverter.Tests
+{
+    [TestFixture]
+    public class RomanConverterTest
+    {
+
+        private RomanConverter _RomanConverter;
+
+        [SetUp] //NUnit Debe de ejecutar este método antes de cada prueba
+        public void Setup()
+        {
+            this._RomanConverter = new RomanConverter();
+        }
+
+        [Test]
+        [TestCase(1, "I")]
+        [TestCase(2, "II")]
+        [TestCase(3, "III")]
+        [TestCase(4, "IV")]
+        [TestCase(5, "V")]
+        [TestCase(6, "VI")]
+        [TestCase(7, "VII")]
+        [TestCase(8, "VIII")]
+        [TestCase(9, "IX")]
+        
+
+        public void Answer_InputEqualValue_OutputCorrect(int input, string expectedOutPut)
+        {
+            string output = this._RomanConverter.Answer(input);
+
+            Assert.AreEqual(expectedOutPut, output);
+        }
+
+    }
+}
---
+++ TDDCourse_IMP0047/RomanConverter/RomanConverter.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanConverter
+{
+    public class RomanConverter
+    {
+        public string Answer(int input)
+        {
+            string[] units = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
+            int unit = (input % 10)-1;
+
+            return units[unit];
+        }
+    }
+}
